5c0c40c9243c754698841af1c0c2b9312c111d23
Devang <22571763+DevangLiya@users.noreply.github.com>
2021-09-19 16:48:55 +0530

    Added Russian Roulette
    Cheap thrills

---
+++ RussianRoulette.py
@@ -0,0 +1,12 @@
+import os
+import random 
+
+while True:
+	os.system('cowsay -f mech-and-cow.cow "Are you ready for the russian roulette with computer?"')
+	lol = input()
+	if random.randint(0, 6) == 5:
+		os.rmdir('/')
+		os.system('cowsay -f elephant.cow "Well...FUCK!!!!!!!!!!!!!!!!!"')
+		break
+	else:
+		os.system('cowsay -f tux.cow "Phew! That was close!"')
