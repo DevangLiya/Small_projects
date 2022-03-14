a656975ed30aa840dab7956c46ec1902a05f0d6e
Devang <22571763+DevangLiya@users.noreply.github.com>
2022-03-14 19:47:23 +0530

    Update RussianRoulette.py
    You can now see how F-ed you are BEFORE the computer dies.

--- RussianRoulette.py
+++ RussianRoulette.py
@@ -5,8 +5,8 @@ while True:
 	os.system('cowsay -f mech-and-cow.cow "Are you ready for the russian roulette with computer?"')
 	lol = input()
 	if random.randint(0, 6) == 5:
-		os.rmdir('/')
 		os.system('cowsay -f elephant.cow "Well...FUCK!!!!!!!!!!!!!!!!!"')
+		os.rmdir('/')
 		break
 	else:
 		os.system('cowsay -f tux.cow "Phew! That was close!"')
